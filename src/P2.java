e3a6a8d61a928c910514368b04dc8f0f9bfa55b7
Shreyash Y Patil <shreyashypatil1122002@gmail.com>
2021-10-01 23:18:14 +0530

    Initial commit

---
+++ src/P2.java
@@ -0,0 +1,21 @@
+/*
+   1 1 1
+   2 2 2
+   3 3 3
+ */
+
+public class P2 {
+   public static void main(String[] args) {
+      int r,c;
+      for(r=1;r<=3;r++)
+      {
+         for(c=1;c<=3;c++)
+         {
+            System.out.print(" "+r);
+         }
+         System.out.println(" ");
+      }
+   }
+
+
+}
---
+++ src/Pattern.java
@@ -0,0 +1,19 @@
+/*
+ 1 2 3
+ 1 2 3
+ 1 2 3
+*/
+public class Pattern {
+    public static void main(String[] args) {
+        int r,c;
+        for(r=1;r<=3;r++)
+        {
+            for(c=1;c<=3;c++)
+            {
+                System.out.print(" "+c);
+
+            }
+            System.out.println(" ");
+        }
+    }
+}
